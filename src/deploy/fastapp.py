243800084f4dd4ce0f4cc64f2f696b09413f7467
tienkng <None>
2024-09-11 14:42:34 +0700

    fix: missing libraries, detach postprocessing and get numpy array segmentation, update function upload to call segment API

--- src/deploy/fastapp.py
+++ src/deploy/fastapp.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from typing import List, List
+from typing import List, Any
 
 import time
 from functools import partial
@@ -10,17 +10,22 @@ import json
 from pydantic import BaseModel
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+from PIL import Image
+import torch
+import io
 
 import tritonclient.grpc as grpcclient
 import tritonclient.http as httpclient
 from tritonclient.utils import InferenceServerException
 
-from src.utils import client
-from src.utils.image import SEGMENT_COLOR
+from projects.interior_design_segmentation.src.utils import client
+from projects.interior_design_segmentation.src.utils.image import SEGMENT_COLOR
 
 #############
 # Initialize
 #############
+load_dotenv()
 #
 model_name    = os.getenv("MODEL_NAME")
 model_version = os.getenv("MODEL_VERSION", "")
@@ -32,7 +37,8 @@ verbose       = os.getenv("VERBOSE", "False").lower() in ("true", "1", "t")
 async_set     = os.getenv("ASYNC_SET", "False").lower() in ("true", "1", "t")
 #
 model_name_or_path = os.getenv('MODEL_NAME_OR_PATH')
-
+artifacts = os.getenv('ARTIFACTS', './datahub/output')
+os.makedirs(artifacts, exist_ok=True)
 
 ############
 # Config
@@ -95,7 +101,7 @@ LABEL = config_file['label2id']
 app = FastAPI()
 #
 class ListImageItem(BaseModel):
-    data: List[np.array]
+    data: List[Any]
 
 
 # Initialize Triton client
@@ -103,9 +109,10 @@ class ListImageItem(BaseModel):
 async def create_upload_file(files: List[UploadFile] = File(...)):
     contents = []
     for file in files:
-        contents.append(await file.file.read())
-
-    return {"filename": [file.filename for file in files]}
+        contents.append(await file.read())
+    item = ListImageItem(data=contents)
+    response = await segment(item)
+    return {"filename": response}
 
 @app.post("/segment")
 async def segment(requests: ListImageItem) -> JSONResponse:
@@ -154,28 +161,64 @@ async def segment(requests: ListImageItem) -> JSONResponse:
     print("Process time: ", end_time - start_time)
     
     # Get outputs
-    outputs = response.as_numpy("pixel_value")
-    outputs = postprocess(segment=outputs, name=["wall", "floor", "ceiling"])
+    outputs1 = response.as_numpy("class_queries_logits")
+    outputs2 = response.as_numpy("masks_queries_logits")
+    segmented = postprocess(
+        outputs=[outputs1, outputs2], 
+        target_sizes=targets_size
+    )
+    finally_segmented = [get_segment(segment, name=['ceiling', 'wall', 'floor']) for segment in segmented]
+
+    path_images = []
+    for idx, e_segment in enumerate(finally_segmented):
+        Image.fromarray(e_segment).save(
+            artifacts + f'/image_segmented_{idx}.jpg'
+        )
+        path_images.append(artifacts + f'/image_segmented_{idx}.jpg')
 
     return JSONResponse(
-        outputs.tolist(), status_code=200
+        path_images, status_code=200
     )
 
 ###################
 # Helper functions
 ###################
 
-def postprocess(segment, name):
-    """Post processing get segmentation mask
+def postprocess(outputs, target_sizes):
+    class_queries_logits = torch.tensor(outputs[0])  # [batch_size, num_queries, num_classes+1]
+    masks_queries_logits = torch.tensor(outputs[1])  # [batch_size, num_queries, height, width]
 
-    Args:
-        segment (np.array): An image mask with shape 2D
-        name (list): name of segment
+    # Remove the null class `[..., :-1]`
+    masks_classes = class_queries_logits.softmax(dim=-1)[..., :-1]
+    masks_probs = masks_queries_logits.sigmoid()  # [batch_size, num_queries, height, width]
 
-    Returns:
-        Image(np.array): Image with segment mask
-    """
-    color_seg = np.zeros((segment.shape[0], segment.shape[1], 3), dtype=np.uint8)
+    # Semantic segmentation logits of shape (batch_size, num_classes, height, width)
+    segmentation = torch.einsum("bqc, bqhw -> bchw", masks_classes, masks_probs)
+    batch_size = class_queries_logits.shape[0]
+
+    # Resize logits and compute semantic segmentation maps
+    if target_sizes is not None:
+        if batch_size != len(target_sizes):
+            raise ValueError(
+                "Make sure that you pass in as many target sizes as the batch dimension of the logits"
+            )
+
+        semantic_segmentation = []
+        for idx in range(batch_size):
+            resized_logits = torch.nn.functional.interpolate(
+                segmentation[idx].unsqueeze(dim=0), size=target_sizes[idx], mode="bilinear", align_corners=False
+            )
+            semantic_map = resized_logits[0].argmax(dim=0)
+            semantic_segmentation.append(semantic_map)
+            break
+    else:
+        semantic_segmentation = segmentation.argmax(dim=1)
+        semantic_segmentation = [semantic_segmentation[i] for i in range(semantic_segmentation.shape[0])]
+
+    return semantic_segmentation
+
+def get_segment(segment, name):
+    color_seg = np.zeros((segment.shape[0], segment.shape[1], 3), dtype=np.uint8) # height, width, 3
     for _name in name:
         assert _name in list(LABEL.keys()), f'Could not found {name} in data'
         color_seg[segment.cpu().numpy() == LABEL[_name], :] = SEGMENT_COLOR[_name]
@@ -186,9 +229,8 @@ def postprocess(segment, name):
     return img_mask
 
 
-def requestGenerator(batched_image_data, input_name, output_names, dtype, FLAGS):
-    protocol = FLAGS.protocol.lower()
-
+def requestGenerator(batched_image_data, input_name, output_names, dtype):
+    
     if protocol == "grpc":
         client = grpcclient
     else:
@@ -203,4 +245,4 @@ def requestGenerator(batched_image_data, input_name, output_names, dtype, FLAGS)
         client.InferRequestedOutput(output_names[1], binary_data=True)
     ]
 
-    yield inputs, outputs, FLAGS.model_name, FLAGS.model_version
+    return inputs, outputs
